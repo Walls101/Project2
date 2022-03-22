e330df242d79dd66634fa74e1c6d8a6fe53efee8
Alex Paredes <alexparedes83@gmail.com>
2022-03-22 14:10:19 -0600

    added film posters and captions

--- films/film-script.js
+++ films/film-script.js
@@ -1,12 +1,33 @@
-import { films } from '../data/films'
+import {films} from "../data/films.js"
 
+console.log(films)
 
+function getLastNumber(url) {
+    const secondToLastLetterOfUrl = url[url.length - 2]
+    return secondToLastLetterOfUrl// return the second to last number from the url property of a film object
+}
 
 // First, get a reference to the main element with the id 'filmList' and store it in a variable
 let filmList = document.querySelector('#filmList')
-// Second, use the document.createElement function/method to create a new img element
-let newImage = document.createElement('img')
-// Third, set the source property of the new image to a valid URL or path
-newImage.src = 'https://starwars-visualguide.com/assets/img/films/6.jpg'
-// Fourth, add the newly created image as a child of the main element
-filmList.appendChild(newImage)
+
+
+// for (initialize variable; condition check; increment variable)
+for (let i = 0; i < films.length; i++) {
+
+    // Second, use the document.createElement function/method to create a new img element
+
+    let figure = document.createElement('figure')
+    let figImage = document.createElement('img')
+    let figCaption = document.createElement('figcaption')
+
+    // Third, set the source property of the new image to a valid URL or path
+
+    let filmNum = getLastNumber(films[i].url)
+    figImage.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg`
+    figCaption.textContent = films[i].title
+
+    // Fourth, add the newly created image as a child of the main element
+    figure.appendChild(figImage)
+    figure.appendChild(figCaption)
+    filmList.appendChild(figure)
+}
