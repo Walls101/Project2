ae76f2f30e1f89a6e70579fb13cd477bd6daea2a
Alex Paredes <alexparedes83@gmail.com>
2022-04-05 11:55:03 -0600

    made congress file

---
+++ congress/congress.js
@@ -0,0 +1,30 @@
+import { senators } from '../data/senators.js'
+import { representatives } from '../data/representatives.js'
+
+const allMembersOfCongress = [...senators, ...representatives]//modern combining of array data... like a genius
+
+const senatorDiv = document.querySelector('.senatorsDiv')
+
+
+
+populateSenatorDiv(simplifiedSenators())
+
+const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => acc.
+seniority > senator.seniority ? acc : senator)
+
+const biggestMissedVotesPct = simplifiedSenators().reduce((acc, senator) =>
+acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
+
+console.log(biggestMissedVotesPct.missedVotesPct)
+
+const biggestVactionerList = simplifiedSenators().filter(senator => senator.
+missedVotesPct === biggestMissedVotesPct.missedVotesPct).map(senator => senator.name).join(", ")
+
+console.log(biggestVactionerList)
+
+
+seniorityHeader.textContent = "The most senior Senator is ${mostSeniorMember.name} and the biggest fans of vacations are ${biggestVactionerList}."
+
+simplifiedSenators().forEach(senator => {
+    if(senator.)
+})
