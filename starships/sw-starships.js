941d5501d760ce4cbf6c31be7c793112131a7581
Alex Paredes <alexparedes83@gmail.com>
2022-04-04 22:20:20 -0600

    updated starships

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,2 +1,22 @@
 import { starships } from "../data/starships.js"
 
+const nav = document.querySelector(".nav")
+const navList = document.querySelector(".navList")
+const shipViewer = document.querySelector(".shipViewer")
+
+function populateNav() {
+    starships.forEach((starship) => {
+        const listItem = document.createElement('li')
+        const anchor = document.createElement('a')
+        anchor.href = '#'
+        listItem.textContent = starship.name
+
+
+        listItem.appendChild(anchor)
+        navList.appendChild(listItem)
+    })
+}
+populateNav()
+function populateShipView () {
+
+}
