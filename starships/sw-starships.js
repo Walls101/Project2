9f57d2b5ee4a8dec8c4c1a529e0a7719e068d2d0
Alex Paredes <alexparedes83@gmail.com>
2022-04-07 01:31:40 -0600

    starships page is working.

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,22 +1,42 @@
 import { starships } from "../data/starships.js"
+import { removeChildren, getLastNumber } from "../utils/index.js"
 
 const nav = document.querySelector(".nav")
 const navList = document.querySelector(".navList")
 const shipViewer = document.querySelector(".shipViewer")
+const modal = document.querySelector('.modal')
+const closeButton  = document.querySelector('.modal-close')
+//To DO!: Add a click  event listener for the modal-background
+const shipMessage = document.querySelector('.box p')
+
+closeButton.addEventListener('click', () => modal.classList.toggle('is-active'))
 
 function populateNav() {
     starships.forEach((starship) => {
         const listItem = document.createElement('li')
         const anchor = document.createElement('a')
         anchor.href = '#'
-        listItem.textContent = starship.name
-
+        anchor.textContent = starship.name
+        anchor.addEventListener('click', () => populateShipView(starship))
 
         listItem.appendChild(anchor)
         navList.appendChild(listItem)
     })
 }
 populateNav()
-function populateShipView () {
 
+function populateShipView (shipData) {
+    removeChildren(shipViewer)
+
+
+    const shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        shipImage.hidden = true
+        shipMessage.textContent = `The ship is known as ${shipData.name} is currently in space port  for repairs`
+        modal.classList.toggle('is-active')
+    })
+
+    shipViewer.appendChild(shipImage)
 }
